a77ddb6df1bfc68582f1f58640444a7ba369a1c8
sahil afrid farookhi <msafarookhi@gmail.com>
2025-01-10 10:41:32 -0800

    fetch api data to table and add local env files to .gitignore

--- src/App.js
+++ src/App.js
@@ -1,75 +1,62 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+const BASE_URL = `https://financialmodelingprep.com/api/v3/income-statement/AAPL?period=annual&apikey=${API_KEY}`;
+
 function App() {
+  const [data, setData] = useState([]);
+
+  const fetchAAPL = async (url) => {
+    if (!API_KEY) {
+      console.error('API key is missing');
+      return;
+    }
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      if (data.length > 0) {
+        setData(data);
+      }
+      console.log('Data:', data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAAPL(BASE_URL);
+  }, []);
+
   return (
-
-
-    <div class="relative overflow-x-auto">
-      <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+    <div className="relative overflow-x-auto">
+      <h1>Financial Data Filtering App</h1>
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <th scope="col" class="px-6 py-3">
-              Product name
-            </th>
-            <th scope="col" class="px-6 py-3">
-              Color
-            </th>
-            <th scope="col" class="px-6 py-3">
-              Category
-            </th>
-            <th scope="col" class="px-6 py-3">
-              Price
-            </th>
+            <th scope="col" className="px-6 py-3">Date</th>
+            <th scope="col" className="px-6 py-3">Revenue</th>
+            <th scope="col" className="px-6 py-3">Net Income</th>
+            <th scope="col" className="px-6 py-3">Gross Profit</th>
+            <th scope="col" className="px-6 py-3">EPS (Earnings Per Share)</th>
+            <th scope="col" className="px-6 py-3">Operating Income</th>
           </tr>
         </thead>
         <tbody>
-          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-              Apple MacBook Pro 17"
-            </th>
-            <td class="px-6 py-4">
-              Silver
-            </td>
-            <td class="px-6 py-4">
-              Laptop
-            </td>
-            <td class="px-6 py-4">
-              $2999
-            </td>
-          </tr>
-          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-              Microsoft Surface Pro
-            </th>
-            <td class="px-6 py-4">
-              White
-            </td>
-            <td class="px-6 py-4">
-              Laptop PC
-            </td>
-            <td class="px-6 py-4">
-              $1999
-            </td>
-          </tr>
-          <tr class="bg-white dark:bg-gray-800">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-              Magic Mouse 2
-            </th>
-            <td class="px-6 py-4">
-              Black
-            </td>
-            <td class="px-6 py-4">
-              Accessories
-            </td>
-            <td class="px-6 py-4">
-              $99
-            </td>
-          </tr>
+          {data.map((item) => (
+            <tr key={item.date} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <td className="px-6 py-4">{item.date}</td>
+              <td className="px-6 py-4">{item.revenue}</td>
+              <td className="px-6 py-4">{item.netIncome}</td>
+              <td className="px-6 py-4">{item.grossProfit}</td>
+              <td className="px-6 py-4">{item.eps}</td>
+              <td className="px-6 py-4">{item.operatingIncome}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
-
   );
 }
 
